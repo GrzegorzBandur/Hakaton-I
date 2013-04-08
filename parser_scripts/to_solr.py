8562811b064c1725f7da33f75734022b192c96b1
Piotr Szczepaniak <ps50366@st.amu.edu.pl>
2013-04-08 23:51:17 +0200

    wrzucanie po 10 wynikow do solr

--- parser_scripts/to_solr.py
+++ parser_scripts/to_solr.py
@@ -4,12 +4,20 @@ import pickle
 import pysolr
 import os
 
-for nazwa_pliku in os.listdir(r'../parsed_results/'):
-    plik = file(r'../parsed_results/%s' % nazwa_pliku)
+katalog = r'../parsed_results/'
+
+for nazwa_pliku in os.listdir(katalog):
+    plik = file('{0}{1}'.format(katalog, nazwa_pliku))
     print 'miele', nazwa_pliku
     dane = pickle.load(plik)
     print 'zaladowano stron:', len(dane)
 
     solr = pysolr.Solr(r'http://127.0.0.1:8983/solr')
-    solr.add(dane)
+    pocz = 0
+    for dyszki in range(10, len(dane), 10):
+        print 'dodaje od {0} do {1}'.format(pocz, dyszki)
+        solr.add(dane[pocz:dyszki])
+        pocz=dyszki
+    print 'dodaje od {0} do konca'.format(pocz)
+    solr.add(dane[pocz:])
     print 'done', nazwa_pliku
