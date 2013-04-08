b6cb160593e4d44f221cf252a0a5954b72bd184f
Piotr Szczepaniak <ps50366@st.amu.edu.pl>
2013-04-08 14:15:51 +0200

    upgrade wygody parsera, mieli wszystkie pliki z podanego katalogu (zmienna w 7 linii)

--- parser_scripts/parser.py
+++ parser_scripts/parser.py
@@ -2,6 +2,9 @@
 
 import re
 import pickle
+import os
+
+wyniki_z_nutch = r'P:/all/'    # katalog z wynikami z nutch
 
 licencje = {
     'freeware': 'freeware',
@@ -110,82 +113,92 @@ licencje_njdz = {
 
 tagi = [r'^Recno:: ([0-9]+)$', r'^URL:: (.*)$', r'^ParseText::$', r'^Content::$', r'^Version: (-?[0-9]+)$', r'^url: (.*)$', r'^base: (.*)$', r'^contentType: (.*)$', r'^metadata: (.*)$', r'^Content:$']
 
-#nutch = file(r'../../../all/all(excluded deviantart).txt')
-nutch = file(r'../../../all/deviantart.txt')
-dane = [{'id':'start'}]
-status = ''
+def odszukaj_licencje(strona):
+    for klucz in licencje.iterkeys():
+        if klucz in strona.get('text', '').lower():
+            strona['license'] = licencje[klucz]
+            print strona['id'], licencje[klucz]
+            return 1
+    for klucz in licencje_njdz.iterkeys():
+        if ('licen' in strona.get('text', '').lower()) and (klucz in strona.get('text', '').lower()):
+            strona['license'] = licencje_njdz[klucz]
+            print strona['id'], licencje_njdz[klucz]
+            return 1
 
-for linia in nutch:
-    #print linia
-    if re.match(tagi[0], linia):
-        #print '-------------nowa strona-------------'
-        for klucz in licencje.iterkeys():
-            if klucz in dane[-1].get('text', '').lower():
-                dane[-1]['license'] = licencje[klucz]
-                print dane[-1]['id'], licencje[klucz]
-                break
-        if not 'license' in dane[-1]:
-            for klucz in licencje_njdz.iterkeys():
-                if ('licen' in dane[-1].get('text', '').lower()) and (klucz in dane[-1].get('text', '').lower()):
-                    dane[-1]['license'] = licencje_njdz[klucz]
-                    print dane[-1]['id'], licencje_njdz[klucz]
-                    break
-        if not 'license' in dane[-1]:
-            #print 'BRAK LICENCJI', dane[-1]['id']
-            dane.pop()
-        status = ''
-        dane.append({})
-        #dane[-1]['recno'] = re.match(tagi[0], linia).groups()[0]
-    elif re.match(tagi[1], linia):
-        #print '-------------url-------------'
-        status = ''
-        dane[-1]['url'] = re.match(tagi[1], linia).groups()[0]
-        dane[-1]['id'] = dane[-1]['url']
-    elif re.match(tagi[4], linia):
-        #print '-------------wersja-------------'
-        status = ''
-        #dane[-1]['_version_'] = 1
-        #dane[-1]['_version_'] = re.match(tagi[4], linia).groups()[0]
-    elif re.match(tagi[5], linia):
-        #print '-------------url-------------'
-        status = ''
-        #dane[-1]['url'] = re.match(tagi[5], linia).groups()[0]
-    elif re.match(tagi[6], linia):
-        #print '-------------base-------------'
-        status = ''
-        #dane[-1]['base'] = re.match(tagi[6], linia).groups()[0]
-    elif re.match(tagi[7], linia):
-        #print '-------------contentType-------------'
-        status = ''
-        #dane[-1]['contentType'] = re.match(tagi[7], linia).groups()[0]
-    elif re.match(tagi[8], linia):
-        #print '-------------metadata-------------'
-        status = ''
-        #dane[-1]['metadata'] = re.match(tagi[8], linia).groups()[0]
-    elif re.match(tagi[2], linia):
-        #print '-------------ParseText-------------'
-        status = 'ParseText'
-        dane[-1]['text'] = ''
-    elif re.match(tagi[3], linia):
-        #print '-------------Content-------------'
-        status = ''
-    elif re.match(tagi[9], linia):
-        #print '-------------Content-------------'
-        status = 'Content'
-        #dane[-1]['Content'] = ''
-    elif status == 'ParseText':
-        #print '-------------ParseText-------------'
-        dane[-1]['text'] += linia
-    elif status == 'Content':
-        #print '-------------Content-------------'
-        #dane[-1]['Content'] += linia
-        pass
-        
-nutch.close()
-print 'wynikow:', len(dane)
-        
-#plik = file(r'../parsed_results/all(excluded deviantart)_parsed_filtered.txt', 'w')
-plik = file(r'../parsed_results/deviantart_parsed_filtered.txt', 'w')
-pickle.dump(dane, plik)
-plik.close()
+
+for nazwa_pliku in os.listdir(wyniki_z_nutch):
+    adres = '{0}{1}'.format(wyniki_z_nutch, nazwa_pliku)
+    nutch = file(adres)
+    print 'miele', adres
+
+    dane = [{'id':'start'}]
+    status = ''
+
+    for linia in nutch:
+        #print linia
+        if re.match(tagi[0], linia):
+            #print '-------------nowa strona-------------'
+            if not odszukaj_licencje(dane[-1]):
+                #print 'BRAK LICENCJI', dane[-1]['id']
+                dane.pop()
+            status = ''
+            dane.append({})
+            #dane[-1]['recno'] = re.match(tagi[0], linia).groups()[0]
+        elif re.match(tagi[1], linia):
+            #print '-------------url-------------'
+            status = ''
+            dane[-1]['url'] = re.match(tagi[1], linia).groups()[0]
+            dane[-1]['id'] = dane[-1]['url']
+        elif re.match(tagi[4], linia):
+            #print '-------------wersja-------------'
+            status = ''
+            #dane[-1]['_version_'] = 1
+            #dane[-1]['_version_'] = re.match(tagi[4], linia).groups()[0]
+        elif re.match(tagi[5], linia):
+            #print '-------------url-------------'
+            status = ''
+            #dane[-1]['url'] = re.match(tagi[5], linia).groups()[0]
+        elif re.match(tagi[6], linia):
+            #print '-------------base-------------'
+            status = ''
+            #dane[-1]['base'] = re.match(tagi[6], linia).groups()[0]
+        elif re.match(tagi[7], linia):
+            #print '-------------contentType-------------'
+            status = ''
+            #dane[-1]['contentType'] = re.match(tagi[7], linia).groups()[0]
+        elif re.match(tagi[8], linia):
+            #print '-------------metadata-------------'
+            status = ''
+            #dane[-1]['metadata'] = re.match(tagi[8], linia).groups()[0]
+        elif re.match(tagi[2], linia):
+            #print '-------------ParseText-------------'
+            status = 'ParseText'
+            dane[-1]['text'] = ''
+        elif re.match(tagi[3], linia):
+            #print '-------------Content-------------'
+            status = ''
+        elif re.match(tagi[9], linia):
+            #print '-------------Content-------------'
+            status = 'Content'
+            #dane[-1]['Content'] = ''
+        elif status == 'ParseText':
+            #print '-------------ParseText-------------'
+            dane[-1]['text'] += linia
+        elif status == 'Content':
+            #print '-------------Content-------------'
+            #dane[-1]['Content'] += linia
+            pass
+            
+    if not odszukaj_licencje(dane[-1]):
+        #print 'BRAK LICENCJI', dane[-1]['id']
+        dane.pop()
+            
+    nutch.close()
+    print 'wynikow:', len(dane)
+            
+    adres = r'../parsed_results/{0}_parsed_filtered.txt'.format(nazwa_pliku)
+    print 'zapisuje w', adres
+    plik = file(adres, 'w')
+    pickle.dump(dane, plik)
+    plik.close()
 
